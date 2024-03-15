4d0931f0830d43687cd0796b26c64aa83a373252
Prisca Piccirilli <priscapiccirilli@users.noreply.github.com>
2024-03-15 10:29:09 +0100

    function finds local maxima

--- 1_testing/hands_on/local_maxima_part2/local_maxima.py
+++ 1_testing/hands_on/local_maxima_part2/local_maxima.py
@@ -7,4 +7,24 @@ def find_maxima(x):
     Output:
     idx -- list of indices of the local maxima in x
     """
-    return []
+    idx = []
+    up = False
+    down = False
+    for i in range(len(x)):
+        if i == 0 or x[i - 1] < x[i]:
+            up = True
+            up_idx = i
+        elif x[i - 1] > x[i]:
+            up = False
+
+        # if x[i-1] == x[i], no change
+
+        if i + 1 == len(x) or x[i + 1] < x[i]:
+            down = True
+        elif x[i + 1] > x[i]:
+            down = False
+
+        if up and down:
+            idx.append(up_idx)
+
+    return idx
