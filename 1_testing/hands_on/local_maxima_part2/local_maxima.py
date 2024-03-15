bf8cbf78c39a510eb3310a54a2b13679e38926b9
frawee <franziska.weeber@web.de>
2024-03-15 10:26:20 +0100

    add local maxima function and tests

--- 1_testing/hands_on/local_maxima_part2/local_maxima.py
+++ 1_testing/hands_on/local_maxima_part2/local_maxima.py
@@ -7,4 +7,16 @@ def find_maxima(x):
     Output:
     idx -- list of indices of the local maxima in x
     """
-    return []
+    unique = [x[i] for i in range(len(x)) if (i==0) or x[i] != x[i-1]]
+    old_indices = [i for i in range(len(x)) if (i==0) or x[i] != x[i-1]]
+   
+    maxima = []
+    if len(unique) >= 2 and unique[1] < unique[0]:
+        maxima.append(0)
+    for i in range(1, len(x) - 1):
+        if unique[i] > unique[i - 1] and unique[i] > unique[i + 1]:
+            maxima.append(old_indices[i])
+
+    if len(unique) >= 2 and unique[-1] > unique[-2]:
+        maxima.append(len(unique) - 1)
+    return maxima
